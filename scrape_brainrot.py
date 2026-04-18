e28f54e57b797ce34aa7e364242699fa012b8bb2
Ben Shirley <benshirley04@gmail.com>
2026-04-18 19:57:04 +1200

    feat: brainrot scraping script complete

--- scrape_brainrot.py
+++ scrape_brainrot.py
@@ -1,3 +1,8 @@
+
+from PIL import Image
+from typing import Any
+import json
+from os import name
 import sys
 from logging import Logger
 
@@ -6,15 +11,22 @@ from bs4 import BeautifulSoup
 
 from src.trading_card import TradingCard
 
+LOGGER = Logger("brainrot scraper")
+
 
 def main():
     output_directory = sys.argv[1]
-    logger = Logger("brainrot scraper")
     BRAINROT_URL = "https://italianbrainrotcharacters.com"
-    scrape_character("https://italianbrainrotcharacters.com/frigo-camelo")
+    # scrape_character(BRAINROT_URL, "/nooo-my-hotspot")
+    cards = parse_all_brainrot(BRAINROT_URL)
+    for card in cards:
+        output_file = f"{output_directory}/{card["name"].replace(" ", "_")}.json"  # ty:ignore[unresolved-attribute]
+        with open(output_file, "w") as file:
+            file.write(json.dumps(card, indent=4))
+            print(f"wrote {card["name"]} to file")
 
 
-def parse_all_brainrot(url: str, logger: Logger | None = None) -> list[TradingCard]:
+def parse_all_brainrot(url: str) -> list[dict[str, Any]]:
 
     res = requests.get(url)
     parser = BeautifulSoup(res.content, "html.parser")
@@ -26,25 +38,73 @@ def parse_all_brainrot(url: str, logger: Logger | None = None) -> list[TradingCa
     print(brainrot_extensions)
     cards = []
     for extension in brainrot_extensions:
-        card = scrape_character(url + extension)
+        card = scrape_character(url, extension)
         if card:
             cards.append(card)
         else:
-            logger.warning(f"Failed to parse brainrot character at {extension}")
+            LOGGER.warning(f"Failed to parse brainrot character at {extension}")
+    return cards
 
 
-def scrape_character(url: str) -> TradingCard | None:
-    res = requests.get(url)
+def scrape_character(base_url: str, extension: str) -> dict[str, Any] | None:
+    res = requests.get(base_url + extension)
     parser = BeautifulSoup(res.content, "html.parser")
-    print(parser.prettify())
-    
-    move_boxes = parser.find_all("div", class_="power-card")
-    for move_box in move_boxes:
-        name = move_box.find("h3").text
-        power_regex = r'⚡ Power: (\d\d)'
-        accuracy_regex = r'🎯 Accuracy: (\d\d)%'
+    parser.prettify()
+
+    meta_dict: dict[str, str] = {}
+    character_meta = parser.find(name="div", class_="character-meta")
+    if character_meta is not None:
+        metadata = character_meta.find_all("li")
+        for metadatum in metadata:
+            key = metadatum.find("strong")
+            val = metadatum.find("div", class_="text-end")
+            if key is not None and val is not None:
+                meta_dict[key.text.strip()] = val.text.strip()
+
+    stat_dict: dict[str, int] = {}
+    character_stats = parser.find(name="section", class_="character-stat")
+    if character_stats is not None:
+        stats = character_stats.find_all("li")
+        for stat in stats:
+            key = stat.find("strong")
+            val = stat.find("span")
+            if key is not None and val is not None:
+                stat_dict[key.text.strip()] = int(val.text.strip())
+    lore_tag = parser.find("section", class_="character-post")
+    lore = None
+    if lore_tag is not None:
+        lore = lore_tag.text
+
+    character_img = parser.find("div", class_="character-img")
+    image = None
+    if character_img is not None:
+        img_elem = character_img.find("img")
+        if img_elem is not None:
+            image_ext = str(img_elem.attrs["src"])
+            image = get_brainrot_image(base_url, image_ext)
 
 
+
+    return {
+        "name":meta_dict.get("Name", "error"),
+        "short_name":meta_dict.get("Short Name", None),
+        "height":meta_dict.get("Height", None),
+        "weight":meta_dict.get("Weight", None),
+        "hp":stat_dict.get("HP", None),
+        "attack":stat_dict.get("Attack", None),
+        "defense":stat_dict.get("Defense", None),
+        "special_attack":stat_dict.get("Special Attack", None),
+        "special_defense":stat_dict.get("Special Defense", None),
+        "speed":stat_dict.get("Speed", None),
+        "lore": lore
+    }
+
+def get_brainrot_image(base_url: str, extension: str) -> bool:
+    res = requests.get(base_url + extension).content
+    filename = extension.split("/")[-1]
+    with open(f"resources/images/{filename}.jpg", "wb") as file:
+        file.write(res)
+    return True
 
 if __name__ == "__main__":
     main()
--- src/trading_card.py
+++ src/trading_card.py
@@ -8,8 +8,8 @@ from src.move import Move
 class TradingCard(BaseModel):
     name: str
     short_name: str
-    height: float
-    weight: float
+    height: str
+    weight: str
 
     hp: int
     attack: int
@@ -18,10 +18,10 @@ class TradingCard(BaseModel):
     special_defense: int
     speed: int
 
-    moves: list[Move]
+    moves: list[Move] | None
 
     @model_validator(mode="after")
     def validate_model(self) -> Self:
-        if len(self.moves) != 4:
+        if self.moves is not None and len(self.moves) != 4:
             raise ValueError("Exactly 4 moves required.")
         return self
